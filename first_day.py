620358426ff64d9568364275ba997b3f332069aa
baba-amir <155196653+aqaamirali@users.noreply.github.com>
2024-10-17 20:59:08 +0330

    Add files via upload

---
+++ first_day.py
@@ -0,0 +1,7 @@
+# let us begin with my very first python code, learning how to print, calculate length and f"strings"
+
+title = "hello, this program will give you the length of your name!"
+print(title)
+name = input("enter your name:\n")
+name_len = len(name)
+print(f"length of your name is {name_len}")
